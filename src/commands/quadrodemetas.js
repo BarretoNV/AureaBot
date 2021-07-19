e635c82fdc917eddcaac6c38001bc3a79eb3cd35
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2021-07-19 20:24:29 -0300

    json funcionand valeu Gom3s

--- src/commands/quadrodemetas.js
+++ src/commands/quadrodemetas.js
@@ -35,23 +35,26 @@ module.exports = {
         .setDescription('Vamos buscar o TRI?')
         .setThumbnail('https://i.ibb.co/xgLnZt4/urea-Selo-Copia.png')
         .addFields(
-            { name: 'Projetos:', value: worksheets.Planilha1 , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: 'Projetos:', value: worksheets.Planilha1[1].Projetos , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].Projetos , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Faturamento:', value: num , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: 'Faturamento:', value: worksheets.Planilha1[1].Faturamento , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].Faturamento , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Ações compartilhadas:', value: num , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: 'Ações compartilhadas:', value: worksheets.Planilha1[1].Acoes_compartilhadas , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].Acoes_compartilhadas , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'NPS:', value: num , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: 'NPS:', value: worksheets.Planilha1[1].NPS , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].NPS , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Participação em eventos:', value: num , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '% de Membros que executam:', value: worksheets.Planilha1[1].Membros_que_executam , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].NPS , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Projetos de impacto:', value: num , inline: true },
-            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '% de Participação em eventos:', value: worksheets.Planilha1[1].Participacao_em_eventos , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].Participacao_em_eventos , inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Projetos de impacto:', value: worksheets.Planilha1[1].Projetos_de_impacto , inline: true },
+            { name: 'Meta:', value: worksheets.Planilha1[0].Projetos_de_impacto , inline: true },
         )
         .setImage('https://i.ibb.co/ss5P36k/Em-busca-do-TRI.png')
         .addField("Desenvolvido com node.js usando a biblioteca discord.js", "[Clique aqui](https://github.com/BarretoNV/AureaBot) para ver o código fonte no github.")
