f905b7a6fe5275d57b73dddfe3f17403ef8a0ed7
Guilherme Barreto <48062197+BarretoNV@users.noreply.github.com>
2021-07-19 17:08:39 -0300

    quadro de metas incluido

--- src/commands/quadrodemetas.js
+++ src/commands/quadrodemetas.js
@@ -5,27 +5,44 @@ const MessageEmbed = require('discord.js').MessageEmbed;
 
 } */
 
+const num = 16;
+const numMeta = 25;
+
 module.exports = {
     name: "metas",
     description: "Retorna o quadro de metas do ano",
     execute (message, args) {
         const embed = new MessageEmbed()
         .setColor('#dbb826')
-        .setTitle('**METAS ANUAIS**')
-        .setURL('https://discord.js.org/')
-        .setAuthor('Diretoria Executiva', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-        .setDescription('Some description here')
-        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .setTitle('**QUADRO de METAS ANUAIS** V0.1 Beta')
+        .setURL('https://portal.brasiljunior.org.br/ejs/aurea/perfil')
+        .setAuthor('Como foram definidas as metas?', 'https://i.ibb.co/qMrpw21/Colagem-png-sem-fundo.png', 'https://drive.google.com/file/d/1DKbI8MYwE8seDS681PhuP-uyqKfKHLjI/view')
+        .setDescription('Vamos buscar o TRI?')
+        .setThumbnail('https://i.ibb.co/xgLnZt4/urea-Selo-Copia.png')
         .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
+            { name: 'Projetos:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Faturamento:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Ações compartilhadas:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'NPS:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Participação em eventos:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Projetos de impacto:', value: num , inline: true },
+            { name: 'Meta:', value: numMeta , inline: true },
         )
-        .addField('Inline field title', 'Some value here', true)
-        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setImage('https://i.ibb.co/ss5P36k/Em-busca-do-TRI.png')
+        .addField("Desenvolvido com node.js usando a biblioteca discord.js", "[Clique aqui](https://github.com/BarretoNV/AureaBot) para ver o código fonte no github.")
         .setTimestamp()
-        .setFooter('GuiBarr3to', 'https://i.imgur.com/wSTFkRM.png');
+        .setFooter('Desenvolvido por Gui Barreto', 'https://i.imgur.com/wSTFkRM.png')
+
 
         message.reply(embed);
     }
